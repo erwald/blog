bd057438dbaaaab83a9de5dea162621160c758f8
Erich Grunewald <erichgrunewald@gmail.com>
2020-12-03 23:15:47 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,69 @@
+const { DateTime } = require("luxon");
+const markdownIt = require("@gerhobbelt/markdown-it");
+const markdownItAnchor = require("@gerhobbelt/markdown-it-anchor");
+const markdownItFootnote = require("@gerhobbelt/markdown-it-footnote");
+const markdownItSup = require("@gerhobbelt/markdown-it-sup");
+const markdownItSub = require("@gerhobbelt/markdown-it-sub");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+module.exports = function (eleventyConfig) {
+  // Copy images & css to site dir.
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
+
+  // Make sure we have readable dates.
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("d MMMM y");
+  });
+
+  // Configure footnotes.
+  // I got this from here (thanks!):
+  // http://dirtystylus.com/2020/06/15/eleventy-markdown-and-footnotes/
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true,
+  })
+    .use(markdownItAnchor, {
+      permalink: true,
+      permalinkClass: "direct-link",
+      permalinkSymbol: "#",
+    })
+    .use(markdownItFootnote)
+    .use(markdownItSup)
+    .use(markdownItSub);
+  markdownLibrary.renderer.rules.footnote_caption = (tokens, idx) => {
+    let n = Number(tokens[idx].meta.id + 1).toString();
+    if (tokens[idx].meta.subId > 0) {
+      n += ":" + tokens[idx].meta.subId;
+    }
+    return n;
+  };
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  // Add filter for extracting the first 2 paragraphs from a string of HTML.
+  eleventyConfig.addNunjucksFilter("abbrev", (str) => {
+    // Remove the title & footnote references.
+    const withoutTitle = str
+      .replace(/<h\d.+<\/h\d>\s+/, "")
+      .replace(/<sup class="footnote-ref">(?!<sup).+<\/sup>/g, "");
+
+    // If there's an opening quote, remove that, too.
+    const withoutTitleAndQuote = withoutTitle.match(/^<blockquote>/)
+      ? withoutTitle.substring(withoutTitle.indexOf("</blockquote>") + 14)
+      : withoutTitle;
+
+    // Return just the first 2 paragraphs.
+    return withoutTitleAndQuote.split(/<\/p>/).slice(0, 2).join("</p>");
+  });
+
+  return {
+    templateFormats: ["md", "njk", "html", "png", "jpg"],
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    pathPrefix: "/",
+  };
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+# My Personal Blog
+
+This is the personal blog of myself, Erich Grunewald. You can visit it at [erichgrunewald.com](https://www.erichgrunewald.com/).
+
+## License
+
+The text content for this site is licensed using [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1). That means you're not allowed to use it without attributing me. And you're not allowed to use it without attaching the same license to your work. Of course you can quote anything in here, so long as you cite your source.
+
+The code for this site is licensed under [MIT](https://en.wikipedia.org/wiki/MIT_License).
+
+## Style Guide
+
+- Use italics for emphasis.
+- Use bold (styled as underlined) for key terms & phrases.
+- Explanatory or clarifying sentences may be put in parentheses and italics.
+- Code snippets & anything that might appear in code may be put in code tags.
